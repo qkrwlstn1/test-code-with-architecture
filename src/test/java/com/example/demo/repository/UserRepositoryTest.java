b5d9330afcd1de6f67cb79cfb34b1dc174dcc0c3
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2025-10-15 11:29:07 +0900

    불필요한 코드 제거 및 리네이밍

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Optional;
 
@@ -12,45 +13,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest(showSql = true)
 @TestPropertySource("classpath:test.properties")
+@Sql("/sql/user-repository-test-data.sql")
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
-    @Test
-    void UserRepository_가_제대로_연결됨(){
-        //given
-        UserEntity ue = UserEntity.builder()
-                .email("aldhfldns@naver.com")
-                .address("Seoul")
-                .nickname("진수")
-                .status(UserStatus.ACTIVE)
-                .certificationCode("aaaaaaaa-aaaaaaa-aaaaaaaaaa")
-                .build();
 
-        //when
-
-        UserEntity result = userRepository.save(ue);
-
-        //than
-        assertNotNull(result.getId());
-    }
 
     @Test
     void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다(){
         //given
-        UserEntity ue = UserEntity.builder()
-                .email("aldhfldns@naver.com")
-                .address("Seoul")
-                .nickname("진수")
-                .status(UserStatus.ACTIVE)
-                .certificationCode("aaaaaaaa-aaaaaaa-aaaaaaaaaa")
-                .build();
-        UserEntity saveUser = userRepository.save(ue);
         //when
 
 
-        Optional<UserEntity> result = userRepository.findByIdAndStatus(saveUser.getId(), UserStatus.ACTIVE);
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
         //than
         assertTrue(result.isPresent());
     }
@@ -59,18 +36,10 @@ class UserRepositoryTest {
     @Test
     void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다(){
         //given
-        UserEntity ue = UserEntity.builder()
-                .email("aldhfldns@naver.com")
-                .address("Seoul")
-                .nickname("진수")
-                .status(UserStatus.ACTIVE)
-                .certificationCode("aaaaaaaa-aaaaaaa-aaaaaaaaaa")
-                .build();
-        userRepository.save(ue);
         //when
 
 
-        Optional<UserEntity> result = userRepository.findByIdAndStatus(2, UserStatus.ACTIVE);
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
 
 
         //than
---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into users (id, email, nickname, address, certification_code, status, last_login_at)
+values (1, 'aldhfldns@naver.com', 'jing', 'Seoul', 'aaaaaaaa-aaaaaa-aaaa-aaaa-aaaaaaa', 'ACTIVE', 0);
